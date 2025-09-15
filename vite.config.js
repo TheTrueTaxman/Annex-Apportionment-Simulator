16d489fd191e119b3a699da200a885c2d0b2e014
TheTrueTaxman <153420173+TheTrueTaxman@users.noreply.github.com>
2025-09-15 01:07:12 -0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/my-vue-app/' // <-- REPLACE with your repo name (trailing slash!)
-})
