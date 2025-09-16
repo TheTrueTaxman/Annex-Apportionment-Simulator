0a68c7e9e5ebb5659ba6c6b8f198a3e6ed005e75
TheTrueTaxman <153420173+TheTrueTaxman@users.noreply.github.com>
2025-09-16 12:40:51 -0700

    Update sim_script.js

--- sim_script.js
+++ sim_script.js
@@ -338,27 +338,25 @@ const App = {
           </tr>
         </thead>
         
-        <draggable v-model="parties" item-key="code" handle=".drag-handle" tag="tbody">
-          <template #item="{ element, index }">
-           <tr class="border-b">
+        <draggable v-model="parties" item-key="code" tag="tbody">
+          <tr v-for="(p, idx) in parties" :key="p.code" class="border-b">
             <td class="text-center drag-handle" style="cursor:grab;">⋮⋮</td>
             <td class="py-2">
-              <input v-model="element.name" class="p-1 border rounded w-full" placeholder="Party name" />
+              <input v-model="p.name" class="p-1 border rounded w-full" placeholder="Party name" />
             </td>
             <td>
-              <input v-model.number="element.votes" type="number" min="0" class="p-1 border rounded w-full" />
+              <input v-model.number="p.votes" type="number" min="0" class="p-1 border rounded w-full" />
             </td>
             <td>
-              <input v-model.number="element.d" type="number" min="0" class="p-1 border rounded w-full" />
+              <input v-model.number="p.d" type="number" min="0" class="p-1 border rounded w-full" />
             </td>
             <td>
-              <input type="checkbox" v-model="element.inAnnex" />
+              <input type="checkbox" v-model="p.inAnnex" />
             </td>
             <td class="text-right">
               <button @click="removeParty(idx)" class="text-red-600">Remove</button>
             </td>
-           </tr>
-          </template>
+          </tr>
         </draggable>
       </table>
       <div class="mt-2">
